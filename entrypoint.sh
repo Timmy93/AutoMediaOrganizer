ca3807ce16a452aa104d891afb2c576f5813c81f
Federico <masomeno2006@gmail.com>
2026-01-22 12:33:25 +0100

    Allow user below 1000

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
+set -e
 
-# Imposta valori di default se non specificati
 PUID=${PUID:-1000}
 PGID=${PGID:-1000}
 
 # Crea il gruppo se non esiste
 if ! getent group appuser > /dev/null 2>&1; then
-    groupadd -g "$PGID" appuser
+    groupadd -g "$PGID" appuser 2>/dev/null || groupadd --badname -g "$PGID" appuser
 fi
 
 # Crea l'utente se non esiste
 if ! getent passwd appuser > /dev/null 2>&1; then
-    useradd -u "$PUID" -g "$PGID" -m -s /bin/bash appuser
+    useradd -u "$PUID" -g "$PGID" -m -s /bin/bash appuser 2>/dev/null || \
+    useradd --badname -u "$PUID" -g "$PGID" -m -s /bin/bash appuser
 fi
 
 # Assegna i permessi sulla directory /app
